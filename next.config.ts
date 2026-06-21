3031aa62ca93a73e34030b8009d00d180eff84f4
Prinsh-85 <prinshagnihotri67@gmail.com>
2026-06-21 12:50:10 +0530

    feat: update next.config.ts to include Turbopack configuration

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
+import path from "path";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const nextConfig: NextConfig & Record<string, any> = {
+  // Set Turbopack root to the project directory (absolute path)
+  turbopack: {
+    root: path.resolve(__dirname),
+  },
 };
 
 export default nextConfig;
